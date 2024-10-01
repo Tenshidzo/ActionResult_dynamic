1211b8f521957d471e1f92fbe2b9c7580240f609
Stryzhakov Bogdan <bogdan24086@gmail.com>
2024-10-01 17:49:52 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActionResult_dynamic.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult CustomHtmlPage()
+        {
+            // Передаем контент в наш HtmlResult
+            string content = "<h1>Добро пожаловать на мою страницу!</h1>";
+            return new HtmlResult(content);
+        }
+    }
+}
---
+++ Results/HtmlResult.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace ActionResult_dynamic.Results
+{
+    private readonly string _content;
+
+    // Конструктор принимает контент, который нужно отобразить в HTML-разметке
+    public HtmlResult(string content)
+    {
+        _content = content;
+    }
+
+    // Переопределение метода ExecuteResult для формирования HTML ответа
+    public override void ExecuteResult(ControllerContext context)
+    {
+        if (context == null)
+            throw new ArgumentNullException(nameof(context));
+
+        // Формируем ответ в виде HTML страницы
+        var response = context.HttpContext.Response;
+        response.ContentType = "text/html";  // Устанавливаем тип контента как HTML
+
+        var sb = new StringBuilder();
+        sb.Append("<!DOCTYPE html>");
+        sb.Append("<html lang=\"en\">");
+        sb.Append("<head>");
+        sb.Append("<meta charset=\"UTF-8\">");
+        sb.Append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+        sb.Append("<title>Custom HTML Result</title>");
+        sb.Append("</head>");
+        sb.Append("<body>");
+        sb.Append(_content);  // Вставляем переданный контент
+        sb.Append("</body>");
+        sb.Append("</html>");
+
+        // Пишем сгенерированную разметку в ответ
+        response.Write(sb.ToString());
+    }
+
+}
